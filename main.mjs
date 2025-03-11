82147df7116da98ca54e8a129d45758b1f5d9280
vdsa7391 <khushboorathore621@gmail.com>
2025-03-11 23:38:51 +0100

    structure_update_half

---
+++ main.mjs
@@ -0,0 +1,130 @@
+function Container(){
+
+
+    function Meme(image, caption_list, score_list ){
+        this.img= image;
+        this.caption_score_list= []
+
+        this.setCaptionList = function(caption_list,score_list){
+
+        }
+
+        // get and set image
+
+        this.getImage= function(){
+
+        }
+
+        this.setImage = function(image){
+
+        }
+
+        // get and set caption list
+
+        this.setCaptionList= function(caption_list){
+
+        }
+
+        this.getCaptionList= function(){
+
+        }
+
+        // get and set score list
+
+        this.setScoreList= function(score_list){
+
+        }
+
+        this.getScoreList= function(){
+
+
+        }
+
+
+    }
+
+
+    function Meme_collection(){
+        this.memes=[];
+
+    }
+
+
+
+    function User(username, password, user_memes, history){
+
+    }
+
+
+
+    function User_collection(){
+
+    }
+
+
+    //  methods for meme collection
+
+    this.addMeme= function(meme){
+
+    }
+
+    this.removeMeme= function(meme){
+
+    }
+
+
+    this.setMemeImg= function(meme, img){
+
+    }
+
+    this.setMemeCaptions= function(meme,captions){
+
+    }
+
+    this.setMemeScore= function(meme,scores){
+
+    }
+
+    this.getMemes= function(){
+
+    }
+
+
+
+    // methods for user
+
+
+    this.addUser= function(user){
+
+    }
+
+    this.removeUser = function(user){
+
+    }
+    
+
+    this.getUser = function(user){
+
+    }
+    
+    this.setUsername= function(user, username){
+
+    }
+
+    this.setUserKey = function(user, key){
+
+    }
+
+    this.getUsers= function(){
+        
+    }
+
+
+
+
+    
+
+
+
+
+}
